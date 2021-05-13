2d2ddf1fa512ce388fa8b17d96a71f52060f54b7
chriskorsak <chris@chriskorsak.com>
2021-05-13 16:15:07 -0700

    refactor js code to make number of squares in grid dynamic using a prompt. remove hard-coded squares. change square background color.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 //DOM VARIABLES
 const easContainerDiv = document.getElementById('easContainer');
 
-//eascontainerDiv width calculated from css width
-const containerWidth = easContainerDiv.offsetWidth;
+// //eascontainerDiv width calculated from css width
+// const containerWidth = easContainerDiv.offsetWidth;
 
 //squares per side
-const squaresPerSide = 3;
+const squaresPerSide = prompt('how many sqs per side?');
 
-//width of each square
-const squareWidth = Math.floor(containerWidth / squaresPerSide);
+//update css grid with squares per side variable
+easContainerDiv.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
+easContainerDiv.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
+
+// //width of each square
+// const squareWidth = Math.floor(containerWidth / squaresPerSide);
 
 //generate squares and add to container
 for (let i = 0; i < squaresPerSide**2; i++) {
@@ -16,8 +20,8 @@ for (let i = 0; i < squaresPerSide**2; i++) {
   let squareDiv = document.createElement('div');
   squareDiv.classList.add('square');
   //specify square width and height
-  squareDiv.style.width = `${squareWidth}px`;
-  squareDiv.style.height = `${squareWidth}px`;
+  // squareDiv.style.width = `${squareWidth}px`;
+  // squareDiv.style.height = `${squareWidth}px`;
 
   //append to container div
   easContainerDiv.appendChild(squareDiv);
