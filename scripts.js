89d3080000b7c971e243dba6b0197687523a891e
chriskorsak <chris@chriskorsak.com>
2021-05-13 19:10:39 -0700

    remove prompt to get resolution, wrap resolution logic into a function called resolution with the dimension as argument. create more dom variables. code reordering for readability.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,36 @@
 //DOM VARIABLES
 const easContainerDiv = document.getElementById('easContainer');
+const resolutionButtons = document.querySelectorAll('button[data-resolution]');
+const clearButton = document.getElementById('clear');
 
-//squares per side
-const squaresPerSide = prompt('how many sqs per side?');
 
-//update css grid with squares per side variable
-easContainerDiv.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
-easContainerDiv.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
-
-//generate squares and add to container div
-for (let i = 0; i < squaresPerSide**2; i++) {
-  //create square and add class
-  let squareDiv = document.createElement('div');
-  squareDiv.classList.add('square');
-
-  //append to container div
-  easContainerDiv.appendChild(squareDiv);
-}
-
+//EVENT LISTENERS
 //add event listener to entire container div
 easContainerDiv.addEventListener('mouseover', changeColor);
+
+//resolution buttons
+// add click event for each resolution button
+// run resolution function with data from button as the resolution argument
+
+
+function resolution(dimension) {
+  //clear out container div squares
+  easContainerDiv.innerHTML = '';
+
+  //update container div with css grid values from dimension
+  easContainerDiv.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+  easContainerDiv.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+
+  //generate squares and add to container div
+  for (let i = 0; i < dimension**2; i++) {
+    //create square and add class
+    let squareDiv = document.createElement('div');
+    squareDiv.classList.add('square');
+
+    //append to container div
+    easContainerDiv.appendChild(squareDiv);
+  }
+}
 
 function changeColor(e) {
   //if target is not the container div...only the squares
