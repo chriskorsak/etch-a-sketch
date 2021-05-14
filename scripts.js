20690fe426629de02e735ad9485c3506fb60f494
chriskorsak <chris@chriskorsak.com>
2021-05-13 17:45:45 -0700

    add mouseover event logic to change square background colors. clear out commented out code no longer needed.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
 //DOM VARIABLES
 const easContainerDiv = document.getElementById('easContainer');
 
-// //eascontainerDiv width calculated from css width
-// const containerWidth = easContainerDiv.offsetWidth;
-
 //squares per side
 const squaresPerSide = prompt('how many sqs per side?');
 
@@ -11,18 +8,21 @@ const squaresPerSide = prompt('how many sqs per side?');
 easContainerDiv.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
 easContainerDiv.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
 
-// //width of each square
-// const squareWidth = Math.floor(containerWidth / squaresPerSide);
-
-//generate squares and add to container
+//generate squares and add to container div
 for (let i = 0; i < squaresPerSide**2; i++) {
   //create square and add class
   let squareDiv = document.createElement('div');
   squareDiv.classList.add('square');
-  //specify square width and height
-  // squareDiv.style.width = `${squareWidth}px`;
-  // squareDiv.style.height = `${squareWidth}px`;
 
   //append to container div
   easContainerDiv.appendChild(squareDiv);
+}
+
+//add event listener to entire container div
+easContainerDiv.addEventListener('mouseover', changeColor);
+
+function changeColor(e) {
+  //if target is not the container div...only the squares
+  if (e.target.id != 'easContainer')
+  e.target.style.backgroundColor = 'teal';
 }
